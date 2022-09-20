93532cc15fb6aafbf0562a1d83a2ad59867369ea
Илья Барашенков <ya.ilya-03@ya.ru>
2022-09-20 15:10:25 +0300

    CSV

--- EKZ/Program.cs
+++ EKZ/Program.cs
@@ -4,14 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.IO;
 
 namespace EKZ
 {
+    
     /// <summary>
     /// 
     /// </summary>
     public class Program
     {
+        public class test
+        {
+            public string s;
+            public void vvod()
+            {
+                StreamReader vvod = new StreamReader("Model.txt");
+                while (!vvod.EndOfStream)
+                {
+                    s = vvod.ReadLine();
+                    Console.WriteLine(s);
+                }
+                vvod.Close();
+            }
+
+            //Вывод введенных данных в csv
+            public void vivod()
+            {
+                string[] lines = new string[] { s };
+                File.WriteAllLines("test.csv", lines);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             int a = 4;
@@ -20,7 +47,7 @@ namespace EKZ
             matmodel mat = new matmodel();
             mat.minus(4, 2);
             TextWriterTraceListener[] listeners = new TextWriterTraceListener[] {
-            new TextWriterTraceListener(@"Z:\Model.txt"),  // путь на созданный txt файл
+            new TextWriterTraceListener("Model.txt"),  // путь на созданный txt файл
             new TextWriterTraceListener(Console.Out)};
             Debug.Listeners.AddRange(listeners);
             Debug.WriteLine("Программа завершена"); //Сообщение  пользователю и вывод результата в файл
@@ -32,6 +59,7 @@ namespace EKZ
                 sum = a + b;
             }
         }
+
         
         
     }
