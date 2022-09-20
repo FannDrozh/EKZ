07c57d4a654e6dc87e7a0164d2cb867691886e4f
Илья Барашенков <ya.ilya-03@ya.ru>
2022-09-20 14:51:06 +0300

    Diagramm

--- EKZ/Program.cs
+++ EKZ/Program.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace EKZ
 {
-    internal class Program
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Program
     {
         static void Main(string[] args)
         {
-
+            int a = 4;
+            int b = 2;
+            Sum();
+            matmodel mat = new matmodel();
+            mat.minus(4, 2);
+            TextWriterTraceListener[] listeners = new TextWriterTraceListener[] {
+            new TextWriterTraceListener(@"Z:\Model.txt"),  // путь на созданный txt файл
+            new TextWriterTraceListener(Console.Out)};
+            Debug.Listeners.AddRange(listeners);
+            Debug.WriteLine("Программа завершена"); //Сообщение  пользователю и вывод результата в файл
+            Debug.Flush();
+            Console.ReadKey();
+            void Sum()
+            {
+                int sum;
+                sum = a + b;
+            }
         }
+        
+        
     }
     public class matmodel
     {
-
+        public void minus(int a, int b)
+        {
+            int minusi = a - b;
+        }
     }
 }
---
+++ TestEKZ/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using EKZ;
+
+namespace TestEKZ
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSum2_2()
+        {
+            EKZ.matmodel prog = new EKZ.matmodel();
+            prog.minus(10, 2);
+        }
+    }
+}
